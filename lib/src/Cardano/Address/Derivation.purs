5c384fdb877d7aa3c332d89c0c019241107bb13c
paolino <paolo.veronelli@gmail.com>
2026-04-09 12:15:00 +0100

    feat: replace JS key derivation with WASM derive command (T018-T019)

--- lib/src/Cardano/Address/Derivation.purs
+++ lib/src/Cardano/Address/Derivation.purs
@@ -7,12 +7,13 @@ module Cardano.Address.Derivation
 
 import Prelude
 
-import Cardano.Address.Bech32 as Bech32
 import Control.Promise (Promise, toAffE)
-import Data.ArrayBuffer.Types (Uint8Array)
+import Data.Either (Either(..))
 import Data.String (joinWith)
 import Effect (Effect)
 import Effect.Aff (Aff)
+import Effect.Aff as Aff
+import Effect.Exception as Exception
 
 data Role
   = UTxOExternal
@@ -30,35 +31,23 @@ type DerivedKeys =
   , stakePublicKeyBech32 :: String
   }
 
-foreign import deriveRootKeyImpl :: String -> Effect (Promise Uint8Array)
-
-foreign import deriveAccountKeyImpl :: Uint8Array -> Int -> Uint8Array
-
-foreign import deriveAddressKeyImpl :: Uint8Array -> Int -> Int -> Uint8Array
-
-foreign import deriveStakeKeyImpl :: Uint8Array -> Uint8Array
-
-foreign import toXPubImpl :: Uint8Array -> Uint8Array
-
-foreign import serializeXPrvImpl :: Uint8Array -> Uint8Array
+foreign import derivePipelineImpl
+  :: (String -> Either String DerivedKeys)
+  -> (DerivedKeys -> Either String DerivedKeys)
+  -> String
+  -> Int
+  -> Int
+  -> Int
+  -> Effect (Promise (Either String DerivedKeys))
 
 derivePipeline :: Array String -> Int -> Role -> Int -> Aff DerivedKeys
 derivePipeline words accountIndex role addressIndex = do
-  rootKey <- toAffE (deriveRootKeyImpl (joinWith " " words))
-  let
-    accountKey = deriveAccountKeyImpl rootKey accountIndex
-    addressKey = deriveAddressKeyImpl accountKey (roleIndex role) addressIndex
-    addressPublicKey = toXPubImpl addressKey
-    stakeKey = deriveStakeKeyImpl accountKey
-    stakePublicKey = toXPubImpl stakeKey
-  pure
-    { rootKeyBech32: Bech32.encode "root_xsk" (serializeXPrvImpl rootKey)
-    , accountKeyBech32: Bech32.encode "acct_xsk" (serializeXPrvImpl accountKey)
-    , addressKeyBech32: Bech32.encode (addressXskPrefix role) (serializeXPrvImpl addressKey)
-    , addressPublicKeyBech32: Bech32.encode (addressXvkPrefix role) addressPublicKey
-    , stakeKeyBech32: Bech32.encode "stake_xsk" (serializeXPrvImpl stakeKey)
-    , stakePublicKeyBech32: Bech32.encode "stake_xvk" stakePublicKey
-    }
+  result <- toAffE (derivePipelineImpl Left Right mnemonic accountIndex (roleIndex role) addressIndex)
+  case result of
+    Right keys -> pure keys
+    Left err -> Aff.throwError (Exception.error err)
+  where
+  mnemonic = joinWith " " words
 
 roleIndex :: Role -> Int
 roleIndex = case _ of
@@ -71,13 +60,3 @@ roleLabel = case _ of
   UTxOExternal -> "External"
   UTxOInternal -> "Internal"
   Stake -> "Stake"
-
-addressXskPrefix :: Role -> String
-addressXskPrefix = case _ of
-  Stake -> "stake_xsk"
-  _ -> "addr_xsk"
-
-addressXvkPrefix :: Role -> String
-addressXvkPrefix = case _ of
-  Stake -> "stake_xvk"
-  _ -> "addr_xvk"
