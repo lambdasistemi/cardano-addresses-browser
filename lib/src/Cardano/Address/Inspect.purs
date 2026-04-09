9cab8e9c8c7dbcb6a861a4111430415be5089ca3
paolino <paolo.veronelli@gmail.com>
2026-04-09 15:32:25 +0100

    fix: use Nullable for Maybe String FFI in Inspect, fixing pattern match crash

--- lib/src/Cardano/Address/Inspect.purs
+++ lib/src/Cardano/Address/Inspect.purs
@@ -4,15 +4,32 @@ module Cardano.Address.Inspect
   , eitherInspectAddress
   ) where
 
+import Prelude
+
 import Control.Promise (Promise, toAffE)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe)
+import Data.Nullable (Nullable, toMaybe)
 import Effect (Effect)
 import Effect.Aff (Aff)
 
 type DetailRow =
   { label :: String
   , value :: String
+  }
+
+type RawAddressInfo =
+  { addressStyle :: String
+  , addressType :: Int
+  , addressTypeLabel :: String
+  , networkTag :: Int
+  , networkTagLabel :: String
+  , stakeReference :: String
+  , spendingKeyHash :: Nullable String
+  , stakeKeyHash :: Nullable String
+  , spendingScriptHash :: Nullable String
+  , stakeScriptHash :: Nullable String
+  , extraDetails :: Array DetailRow
   }
 
 type AddressInfo =
@@ -30,10 +47,27 @@ type AddressInfo =
   }
 
 foreign import inspectAddressWasmImpl
-  :: (String -> Either String AddressInfo)
-  -> (AddressInfo -> Either String AddressInfo)
+  :: (String -> Either String RawAddressInfo)
+  -> (RawAddressInfo -> Either String RawAddressInfo)
   -> String
-  -> Effect (Promise (Either String AddressInfo))
+  -> Effect (Promise (Either String RawAddressInfo))
+
+toAddressInfo :: RawAddressInfo -> AddressInfo
+toAddressInfo raw =
+  { addressStyle: raw.addressStyle
+  , addressType: raw.addressType
+  , addressTypeLabel: raw.addressTypeLabel
+  , networkTag: raw.networkTag
+  , networkTagLabel: raw.networkTagLabel
+  , stakeReference: raw.stakeReference
+  , spendingKeyHash: toMaybe raw.spendingKeyHash
+  , stakeKeyHash: toMaybe raw.stakeKeyHash
+  , spendingScriptHash: toMaybe raw.spendingScriptHash
+  , stakeScriptHash: toMaybe raw.stakeScriptHash
+  , extraDetails: raw.extraDetails
+  }
 
 eitherInspectAddress :: String -> Aff (Either String AddressInfo)
-eitherInspectAddress value = toAffE (inspectAddressWasmImpl Left Right value)
+eitherInspectAddress value = do
+  result <- toAffE (inspectAddressWasmImpl Left Right value)
+  pure (map toAddressInfo result)
